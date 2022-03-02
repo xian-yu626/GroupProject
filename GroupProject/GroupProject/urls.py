49431253839f9492a16a64f7640fce2da46be147
Rafe <rafebennett22@gmail.com>
2022-03-02 16:50:58 +0000

    added url to dogWalkers page

--- GroupProject/GroupProject/urls.py
+++ GroupProject/GroupProject/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
+    path('DogWalkers/', include('dogWalkers.urls')),
 ]
