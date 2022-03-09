4d476143afb3ecea33dfc050c8fa67f41f24a34f
Rafe <rafebennett22@gmail.com>
2022-03-09 14:39:41 +0000

    Added tests for dog walkers' model file

--- GroupProject/dogWalkers/models.py
+++ GroupProject/dogWalkers/models.py
@@ -24,6 +24,51 @@ class DogWalker(models.Model):
 	
 	def createUser(self):
 		self.save()
-	
+		
 	def __str__(self):
 		return self.name
+		
+	def getName(self):
+		return self.name
+		
+	def getEmail(self):
+		return self.email
+		
+	def getPostcode(self):
+		return self.postcode
+		
+	def getPrice(self):
+		return self.price
+		
+	def getUsrInfo(self):
+		return self.usr_info
+		
+	def get_Avbl_From(self):
+		return self.avbl_from
+		
+	def get_Avbl_To(self):
+		return self.avbl_to
+		
+	def get_Avbl_Morn(self):
+		return self.avbl_morn
+		
+	def get_Avbl_Aftn(self):
+		return self.avbl_aftn
+		
+	def get_Avbl_Eve(self):
+		return self.avbl_eve
+		
+	def get_Acpt_7k(self):
+		return self.acpt_7k
+		
+	def get_Acpt_18k(self):
+		return self.acpt_18k
+		
+	def get_Acpt_45k(self):
+		return self.acpt_45k
+		
+	def get_Acpt_Abv_45k(self):
+		return self.acpt_abv_45k
+		
+	def get_Acpt_Pup(self):
+		return self.acpt_pup
