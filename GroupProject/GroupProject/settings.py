d4af6f95a9a34f4991cc3e8e6c23fe7f1e79845d
Rafe <rafebennett22@gmail.com>
2022-03-10 20:31:20 +0000

    Modified settings for deployment

--- GroupProject/GroupProject/settings.py
+++ GroupProject/GroupProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u5@o-qk8p_xwhwaq4xpa3o-&p=-b6vtx&*%!spgo%#q*9$ro)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
